d8536da505ac1b210d36588409e6cb231ecb9c1b
Morteza <mortezamirzai120@gmail.com>
2019-02-24 21:41:10 +0330

    Make Tester for Lab class

---
+++ src/LabTest.java
@@ -0,0 +1,16 @@
+public class LabTest {
+    public static void main(String[] args){
+        Lab ap = new Lab(2, 30);
+
+        Student morteza = new Student("Morteza", "Mirzai", "9731064");
+        morteza.setGrade((float)19.3);
+
+        System.out.println(morteza.getInfo());
+
+        //enroll someone
+        ap.enrollStudent(morteza);
+
+        System.out.println("Current capacity is " + ap.getCurrentCapacity()
+                + " On " + ap.getDayOfTheWeek() + " days");
+    }
+}
