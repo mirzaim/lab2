e8caa8f5619c28d4820f8629355e60f720e5db0b
Morteza <mortezamirzai120@gmail.com>
2019-02-24 20:59:21 +0330

    Add methods of Student class

--- src/Student.java
+++ src/Student.java
@@ -1,3 +1,46 @@
 public class Student {
-    
+    private String firstName;
+    private String lastName;
+    private String studentID;
+    private float grade;
+
+    public Student(String firstName, String lastName, String studentID){
+
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setStudentID(String studentID) {
+        this.studentID = studentID;
+    }
+
+    public void setGrade(float grade){
+        this.grade = grade;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getStudentID() {
+        return studentID;
+    }
+
+    public float getGrade() {
+        return grade;
+    }
+
+    public String getInfo(){
+        return "";
+    }
 }
